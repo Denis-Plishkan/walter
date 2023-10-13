4b9b3fd7e6014d4601c65d660eb5a6a46ba96d0e
Denis Plishkan <denchik.plishkan@gmail.com>
2023-10-13 17:34:43 +0300

    work with swiper

--- src/js/app.js
+++ src/js/app.js
@@ -67,5 +67,6 @@ isWebp()
 // =======================================================================================================
 
 import burger from './modules/burger.js'
+import heroSwiper from './modules/swiper.js'
 
 
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -3,45 +3,15 @@ import "swiper/swiper-bundle.min.css";
 Swiper.use([Navigation, Pagination, Autoplay, Scrollbar]);
 
 
-function thesisSwiper() {
-  let swiper = new Swiper(".thesis__swiper", {
-    spaceBetween: 24,
-    slidesPerview: 'auto',
+function heroSwiper() {
+  let swiper = new Swiper(".hero__swiper", {
+    // spaceBetween: 24,
+    direction: 'vertical',
+    // slidesPerview: 'auto',
     loop: true,
-    autoplay: {
-      delay: 1500,
-    },
-
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 260,
-      },
-      375: {
-        slidesPerView: 2,
-        spaceBetween: 248
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-      },
-      991: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-      },
-      1440: {
-        slidesPerView: 3,
-        centeredSlides: true,
-        spaceBetween: 0,
-      },
-      1920: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-      },
-    },
   });
 }
 
-thesisSwiper();
+heroSwiper();
 
 
